5e6d2cddca2537eb36f57d867c4eea44cc780a6b
Lucas Diaz <ldiaz@DH.digitalhouse.com>
2020-08-29 00:55:24 -0300

    Toda la conexión con sequelize hecha.

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,12 +5,15 @@ const uploadProduct = require('../middlewares/multerUploadProduct');
 const productValidation = require('../validations/productValidation');
 const modifyProductValidation = require('../validations/modifyProductValidation');
 
+
+// 0 - PRUEBA DE BASE DE DATOS
+router.get('/prueba', productsController.prueba)
 // 1 - Listado de productos
 router.get('/', productsController.index);
 // 2 - Formulario de creacion de productos
 router.get('/create', productsController.uploadForm);
 // 3 - Accion de creacion de producto
-router.post('/', uploadProduct.any(), productValidation, productsController.create);
+router.post('/crear', /*uploadProduct.any(), productValidation,*/ productsController.create);
 // 4 - Detalle de un producto en particular
 router.get('/:productId', productsController.detail);
 // 5 - Formulario de edicion de producto
