e5e5a7b84f47b68e37f38771b60c81035260d64a
Lucas Diaz <ldiaz@DH.digitalhouse.com>
2020-09-22 23:26:55 -0300

    Realizando pruebas de login

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -15,100 +15,106 @@ let usersPARSED;
 // Contador de usuarios para que el ID se agregue automaticamente
 let lastID = 0;
 for (let i = 0; i < usersPARSED.length; i++) {
-  if (usersPARSED[i].userID > lastID) {
-    lastID = usersPARSED[i].userID
-  }
+   if (usersPARSED[i].userID > lastID) {
+      lastID = usersPARSED[i].userID
+   }
 }
 
 const usersController = {
-  showLogin: (req, res) => {
-    res.render('login-form')
-  },
-  logUser: (req, res) => {
-    let errors = validationResult(req);
-    if (errors.isEmpty()) {
-      db.User.findOne({
-        where: {
-          email: req.body.email
-        }
-      })
-        .then(function (result) {
-          if (bcrypt.compareSync(req.body.password, result.password)) {
-            req.session.loggedUser = {
-              name: result.name,
-              email: result.email,
-              category: result.category
+   showLogin: (req, res) => {
+      res.render('login-form')
+   },
+   logUser: (req, res) => {
+      let errors = validationResult(req);
+      if (errors.isEmpty()) {
+         db.User.findOne({
+            where: {
+               email: req.body.email
             }
-            if (req.body.rememberMe == "on") {
-              res.cookie('rememberMe', req.session.loggedUser.email, { maxAge: 1000 * 30 });
-            }
-            res.redirect("/")
-          } else {
-            return res.render("login-form", {
-              errors: errors.mapped()
+         })
+            .then(function (result) {
+               if (bcrypt.compareSync(req.body.password, result.password)) {
+                  db.Product.findAll()
+                     .then(function (productos) {
+                        req.session.loggedUser = {
+                           name: result.name,
+                           email: result.email,
+                           category: result.category
+                        }
+                        let usuario = req.session.loggedUser;
+                        if (req.body.rememberMe == "on") {
+                           res.cookie('rememberMe', req.session.loggedUser.email, { maxAge: 1000 * 30 });
+                        }
+
+                        res.render("index", { usuario: usuario, products: productos })
+                     })
+               } else {
+                  return res.render("login-form", {
+                     errors: errors.mapped(),
+                     usuario: usuario
+                  })
+               }
             })
-          }
-        })
-    } else {
-      // return res.send(errors.mapped())
-      res.render("login-form", {
-        errors: errors.mapped()
-      })
-    }
+      } else {
+         // return res.send(errors.mapped())
+         res.render("login-form", {
+            errors: errors.mapped()
+         })
+      }
 
-    // let errors = validationResult(req);
-    // if (errors.isEmpty()) {
-    //   let userToLog;
-    //   for (let i = 0; i < usersPARSED.length; i++) {
-    //     if (usersPARSED[i].userEmail == req.body.userEmail && bcrypt.compareSync(req.body.userPassword, usersPARSED[i].userPassword)) {
-    //       userToLog = usersPARSED[i];
-    //       break;
-    //     }
-    //   }
-    //   if (userToLog == undefined) {
-    //     return res.render('login-form', {
-    //       errors: [{ msg: 'Credenciales incorrectas' }]
-    //     });
-    //   }
-    //   req.session.loggedUser = userToLog;
-    //   if (req.body.rememberMe == "on") {
-    //     res.cookie('rememberMe', userToLog.userEmail, { maxAge: 1000 * 30 });
-    //   }
-    //   res.send('Usuario logeado con exito')
-    // } else {
-    //   return res.render('login-form', {
-    //     errors: errores.mapped()
-    //   });
-    // }
+      // let errors = validationResult(req);
+      // if (errors.isEmpty()) {
+      //   let userToLog;
+      //   for (let i = 0; i < usersPARSED.length; i++) {
+      //     if (usersPARSED[i].userEmail == req.body.userEmail && bcrypt.compareSync(req.body.userPassword, usersPARSED[i].userPassword)) {
+      //       userToLog = usersPARSED[i];
+      //       break;
+      //     }
+      //   }
+      //   if (userToLog == undefined) {
+      //     return res.render('login-form', {
+      //       errors: [{ msg: 'Credenciales incorrectas' }]
+      //     });
+      //   }
+      //   req.session.loggedUser = userToLog;
+      //   if (req.body.rememberMe == "on") {
+      //     res.cookie('rememberMe', userToLog.userEmail, { maxAge: 1000 * 30 });
+      //   }
+      //   res.send('Usuario logeado con exito')
+      // } else {
+      //   return res.render('login-form', {
+      //     errors: errores.mapped()
+      //   });
+      // }
 
-  },
-  showRegister: (req, res) => {
-    res.render('register')
-  },
-  createUser: (req, res) => {
-    let errors = validationResult(req);
-    if (errors.isEmpty()) {
-      db.User.create({
-        name: req.body.name,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 10),
-        avatar: (req.files[0] != undefined) ? req.files[0].filename : 'defaultUserAvatar.png'
-      })
-        .then(function (result) {
-          res.redirect("/")
-        })
-    } else {
-      res.render("register", {
-        errors: errors.mapped()
-      })
-    }
-  },
-  userLogOut: (req, res) => {
-    req.session.destroy();
-    res.cookie('rememberMe', '', { maxAge: -1 })
-    res.send('Te deslogueaste con éxito')
-  }
+   },
+   showRegister: (req, res) => {
+      res.render('register')
+   },
+   createUser: (req, res) => {
+      let errors = validationResult(req);
+      if (errors.isEmpty()) {
+         db.User.create({
+            name: req.body.name,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10),
+            avatar: (req.files[0] != undefined) ? req.files[0].filename : 'defaultUserAvatar.png'
+         })
+            .then(function (result) {
+               res.redirect("/")
+            })
+      } else {
+         res.render("register", {
+            errors: errors.mapped()
+         })
+      }
+   },
+   userLogOut: (req, res) => {
+      req.session.destroy();
+      res.cookie('rememberMe', '', { maxAge: -1 })
+      res.send('Te deslogueaste con éxito')
+   }
 }
 
 module.exports = usersController;
--- src/middlewares/rememerMeMiddleware.js
+++ src/middlewares/rememerMeMiddleware.js
@@ -1,25 +1,24 @@
 const fs = require('fs');
 const path = require('path');
+const db = require('../database/models')
 
 let rememberMeMiddleware = (req, res, next) => {
-  if (req.cookies.rememberMe != undefined && req.session.loggedUser == undefined) {
-    let usersJSON = fs.readFileSync(path.join(__dirname, '../data/users.json'), 'utf-8');
-    let usersPARSED;
-    if (usersJSON == '') {
-      usersPARSED = [];
-    } else {
-      usersPARSED = JSON.parse(usersJSON)
-    }
-    let userToLog;
-    for (let i = 0; i < usersPARSED.length; i++) {
-      if (usersPARSED[i].userEmail == req.cookies.rememberMe) {
-        userToLog = usersPARSED[i];
-        break;
-      }
-    }
-    req.session.usuarioLogueado = userToLog;
-  }
-  next();
+   if (req.cookies.rememberMe != undefined && req.session.loggedUser != undefined) {
+
+      db.User.findOne({
+         where: {
+            email: req.cookies.rememberMe
+         }
+      })
+         .then(function (result) {
+            req.session.usuarioLogueado = result.email
+            console.log(req.session.usuarioLogueado);
+            let usuarioLogueado = req.session.usuarioLogueado
+            console.log(`+***************************************************+${usuarioLogueado}`);
+         })
+   }
+
+   next();
 }
 
 module.exports = rememberMeMiddleware;
